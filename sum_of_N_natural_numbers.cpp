6afc53abc8d2bd9be1f499a81201b72dbb9d94db
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2022-04-14 10:54:20 +0530

    Add files via upload

---
+++ sum_of_N_natural_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    float sum;
+    cout<<"PLEASE ENTER THE VALUE OF THE N"<<endl;
+    cin>>N;
+    sum=(N*float(N+1)/2);
+    cout<<("THE SUM OF N NATURAL NUMDERS IS:")<<endl;
+    cout<<sum;
+}
