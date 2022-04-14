285438111c11a6bd09b9181648ba40bdf3040dfb
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2022-04-14 12:00:07 +0530

    Add files via upload

---
+++ prime number checker.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i;
+    cout<<"pls enter the number to check"<<endl;
+    cin>>n;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+          cout<<"not a prime"<<endl;
+          break;
+        }
+
+    }
+    if(i==n)
+    {
+        cout<<"prime  number"<<endl;
+    }
+
+    return 0;
+}
